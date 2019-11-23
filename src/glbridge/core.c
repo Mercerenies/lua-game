1803e03ea68ed82527d24159e1ea278b44544b5b
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-11-22 20:24:58 -0500

    Fix for alpha values

--- src/glbridge/core.c
+++ src/glbridge/core.c
@@ -9,6 +9,10 @@ void glbridge_init_and_display(int* argcp, char** argv) {
   glutInitWindowPosition(100, 100);
   glutInitWindowSize(500, 500);
   glutCreateWindow("Hello World!");
+
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glEnable( GL_BLEND );
+  glClearColor(0.0,0.0,0.0,0.0);
 }
 
 void glbridge_main_loop() {
