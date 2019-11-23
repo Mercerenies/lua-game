839c11770b3847d1dcf917a7f5a63fcb0fdcb624
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-11-22 20:25:03 -0500

    Beginnings of a simple snake-like game

--- script/script.lua
+++ script/script.lua
@@ -1,66 +1,75 @@
 
 math.randomseed(os.time())
 
-MyObject = Object:define()
+SnakeHead = Object:define()
 
-function MyObject:draw()
-  local radius = 100
-  Canvas.set_color(Color(1, 0, 0))
-  Canvas.draw_primitive(Constant.TRIANGLES, {
-    self.center,
-    self.center + Vector2(radius * math.cos(self.angle), radius * math.sin(self.angle)),
-    self.center + Vector2(radius * math.cos(self.angle + 0.1), radius * math.sin(self.angle + 0.1))
+local snake_head_keys = {
+  [Key.LEFT] = math.pi,
+  [Key.of('a')] = math.pi,
+  [Key.UP] = 3 * math.pi / 2,
+  [Key.of('w')] = 3 * math.pi / 2,
+  [Key.DOWN] = math.pi / 2,
+  [Key.of('s')] = math.pi / 2,
+  [Key.RIGHT] = 0,
+  [Key.of('d')] = 0,
+}
+
+function SnakeHead:init(t)
+  self.pos = Vector2(t.x, t.y)
+  self.dir = 0
+  self.mov_count = 15
+  self.mov_timer = 0
+  self.trail_len = 5
+  self.trail = {}
+  self.trail_ptr = 0
+  self.score = 0
+end
+
+function SnakeHead:step()
+  self.mov_timer = self.mov_timer + 1
+  if self.mov_timer >= self.mov_count then
+    self.mov_timer = 0
+    self.trail[self.trail_ptr] = self.pos
+    self.trail_ptr = (self.trail_ptr + 1) % self.trail_len
+    self.pos = self.pos + Vector2(16, 0):rotated(self.dir)
+
+    self.score = self.score + 1
+    if self.score % 20 == 0 then
+      if self.mov_count > 5 then
+        self.mov_count = self.mov_count - 1
+      end
+      self.trail_len = self.trail_len + 1
+    end
+
+  end
+end
+
+function SnakeHead:key_down(k)
+  if snake_head_keys[k] then
+    self.dir = snake_head_keys[k]
+  end
+end
+
+function SnakeHead:draw()
+  --Canvas.set_color(Color(0, 0, 0))
+  --Canvas.draw_primitive(Constant.QUADS,
+  --                      {Vector2(0, 0), Vector2(500, 0), Vector2(500, 500), Vector2(0, 500)})
+  Canvas.set_color(Color(1, 1, 1))
+  Canvas.draw_primitive(Constant.QUADS, {
+    self.pos, self.pos + Vector2(15, 0),
+    self.pos + Vector2(15, 15), self.pos + Vector2(0, 15)
   })
-end
-
-function MyObject:init(t)
-  self.angle = t.angle
-  self.center = Window.get_size() / 2
-end
-
-function MyObject:step()
-  self.angle = self.angle - 0.1
-  local moving = false
-  local dir = Vector2(0, 0)
-  if Keyboard.is_down(Key.LEFT) then
-    dir = dir + Vector2(-1, 0)
-    moving = true
-  end
-  if Keyboard.is_down(Key.RIGHT) then
-    dir = dir + Vector2(1, 0)
-    moving = true
-  end
-  if Keyboard.is_down(Key.UP) then
-    dir = dir + Vector2(0, -1)
-    moving = true
-  end
-  if Keyboard.is_down(Key.DOWN) then
-    dir = dir + Vector2(0, 1)
-    moving = true
-  end
-  dir = math.atan2(dir.y, dir.x)
-  if moving then
-    self.center = self.center + 2 * Vector2(math.cos(dir), math.sin(dir))
+  for i = 0, self.trail_len - 1 do
+    local alpha = i / (2 * self.trail_len) + 0.5
+    Canvas.set_color(Color(1, 1, 1, alpha))
+    local pos = self.trail[(self.trail_ptr + i) % self.trail_len]
+    if pos then
+      Canvas.draw_primitive(Constant.QUADS, {
+        pos, pos + Vector2(15, 0),
+        pos + Vector2(15, 15), pos + Vector2(0, 15)
+      })
+    end
   end
 end
 
-local angle = math.random() * 2 * math.pi
-for i=1,4 do
-  local obj = MyObject:new { angle = angle + i * math.pi / 2 }
-  table.insert(Room.get_objects(), obj)
-end
-
---[[
-print(Vector2(1, 2))
-print(Vector2(1, 1) + Vector2(1, 1))
-print(Vector2(1, 1) - Vector2(2, 2))
-print(Vector2(1, 1) * Vector2(1, 1))
-print(Vector2(1, 1) / 2)
-print(Vector2(1, 1) * 2)
-print(2 * Vector2(1, 1))
-print(- Vector2(1, 1))
-print(Vector2(1, 1) == Vector2(1, 1))
-print(Vector2(1, 1).x)
-print(Vector2(1, 1).y)
-print(Vector2(1, 1).gibberish)
---]]
+table.insert(Room.get_objects(), SnakeHead:new({x=128, y=128}))
